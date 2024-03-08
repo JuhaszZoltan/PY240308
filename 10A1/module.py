d8c97fcda16fd6a984248c066a8462cce9061d0b
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-03-08 08:37:10 +0100

    first commit

---
+++ 10A1/module.py
@@ -0,0 +1,8 @@
+class Jatekos:
+    def __init__(self, sor:str) -> None:
+        v:list[str] = sor.strip().split(';')
+        self.mezszam:int = int(v[0])
+        self.nev:str = v[1]
+        self.nemzetiseg:str = v[2]
+        self.poszt:str = v[3]
+        self.szulev:int = int(v[4])
